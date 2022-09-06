4f38c8c99cff0c511dac0c268cd39ac778da471b
Bláj3n <ulrichbence15@freemail.hu>
2022-09-06 18:51:09 +0200

    Gondolatolvasó program alapállapot

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gondolatolvasó
+
+Ez a program kitalálja, hogy a 21 lap közül a játékos melyikre gondolt.
---
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -0,0 +1,13 @@
+package gondolatolvaso;
+
+public class Gondolatolvaso {
+
+    public static int[] oszlop1 = new int[7];
+    public static int[] oszlop2 = new int[7];
+    public static int[] oszlop3 = new int[7];
+
+    public static void main(String[] args) {
+        
+    }
+
+}
