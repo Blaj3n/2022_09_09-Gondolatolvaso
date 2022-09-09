a256ab0fbfd7fe71e6f066abbe929667edcd160d
Bláj3n <ulrichbence15@freemail.hu>
2022-09-09 13:57:50 +0200

    Kártya pakli tömb feltöltése félig

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -1,8 +1,10 @@
 package gondolatolvaso;
 
+import java.util.Arrays;
+
 public class Gondolatolvaso {
 
-    public static String[] pakli = new String[21]; 
+    public static String[] pakli = new String[21];
     public static int[] oszlop1 = new int[7];
     public static int[] oszlop2 = new int[7];
     public static int[] oszlop3 = new int[7];
@@ -10,18 +12,20 @@ public class Gondolatolvaso {
     public static String ertekek[] = {"VIII", "IX", "X", "ALS", "KIR", "ASZ"};
 
     public static void main(String[] args) {
-
+        keveres();
     }
 
     public static String[] keveres() {
-        String tomb[] = {""};
         for (int i = 0; i < szinek.length; i++) {
-            tomb[i] = 
-        }
-        for (int j = 0; j < ertekek.length; j++) {
+            for (int j = 0; j < ertekek.length; j++) {
+                pakli[i] = szinek[i];
+                pakli[j] = szinek[i] + "_" + ertekek[j];
+            }
 
         }
-        return tomb;
+
+        System.out.println(Arrays.toString(pakli));
+        return pakli;
     }
 
 }
