606097d94569c6e0fa5424dd8403ed528b5ecbd4
Bláj3n <ulrichbence15@freemail.hu>
2022-09-09 12:55:18 +0200

    Kártya class törlése

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -2,21 +2,24 @@ package gondolatolvaso;
 
 public class Gondolatolvaso {
 
+    public static String[] pakli = new String[21]; 
     public static int[] oszlop1 = new int[7];
     public static int[] oszlop2 = new int[7];
     public static int[] oszlop3 = new int[7];
-    public static String szinek[] = {"P","T","Z","M"};
-    public static String ertekek[] = {"VIII","IX","X","ALS","KIR","ASZ"};
-    
+    public static String szinek[] = {"P", "T", "Z", "M"};
+    public static String ertekek[] = {"VIII", "IX", "X", "ALS", "KIR", "ASZ"};
 
     public static void main(String[] args) {
-        
+
     }
-    
-    public static String[] keveres(){
+
+    public static String[] keveres() {
         String tomb[] = {""};
-        for (int i = 0; i < enum[szinek]; i++) {
-            
+        for (int i = 0; i < szinek.length; i++) {
+            tomb[i] = 
+        }
+        for (int j = 0; j < ertekek.length; j++) {
+
         }
         return tomb;
     }
