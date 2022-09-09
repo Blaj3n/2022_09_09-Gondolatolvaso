2b5e13f782a65f045eadcb710fcdcee74b9dd2d4
Bláj3n <ulrichbence15@freemail.hu>
2022-09-09 16:00:14 +0200

    feltölt metódus és kiírás metódus, majdnem kész program

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -1,6 +1,5 @@
 package gondolatolvaso;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Gondolatolvaso {
@@ -11,10 +10,10 @@ public class Gondolatolvaso {
     public static int[] oszlop3 = new int[7];
 
     public static void main(String[] args) {
+        feltolt();
         kiiras();
         for (int i = 0; i < 3; i++) {
-            tipp();
-            keveres();
+            keveres(tipp());
             kiiras();
         }
         System.out.println(pakli[11]);
@@ -22,25 +21,35 @@ public class Gondolatolvaso {
 
     public static int tipp() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Melyik oszlopban van a kártyád?");
+        System.out.print("Melyik oszlopban van a kártyád? ");
         int tipp = sc.nextInt();
         return tipp;
     }
 
-    public static String[] kiiras() {
-        for (int i = 0; i < Szinek.values().length; i++) {
-            for (int j = 0; j < Ertekek.values().length; j++) {
-                pakli[i * Ertekek.values().length + j] = Szinek.values()[i] + "_" + Ertekek.values()[j];
+    public static void feltolt() {
+        Szinek[] s = Szinek.values();
+        Ertekek[] e = Ertekek.values();
+        for (int i = 0; i < s.length; i++) {
+            for (int j = 0; j < e.length; j++) {
+                pakli[i * e.length + j] = s[i] + "_" + e[j];
             }
 
         }
+    }
+    
 
-        System.out.println(Arrays.toString(pakli));
-        return pakli;
+    private static void keveres(int tipp) {
+
     }
 
-    private static void keveres() {
-
+    private static void kiiras() {
+        for (int i = 0; i < pakli.length; i++) {
+            System.out.print(pakli[i] + "\t");
+            if(i % Szinek.values().length == Szinek.values().length -1){
+                System.out.println("");
+            }
+        }
+        System.out.println(" ");
     }
 
 }
