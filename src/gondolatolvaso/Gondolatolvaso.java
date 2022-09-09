0b494d4b12976b80e1b9326503850b0d4cabd322
Bláj3n <ulrichbence15@freemail.hu>
2022-09-09 15:18:38 +0200

    kész keverés metódus

--- src/gondolatolvaso/Gondolatolvaso.java
+++ src/gondolatolvaso/Gondolatolvaso.java
@@ -8,8 +8,8 @@ public class Gondolatolvaso {
     public static int[] oszlop1 = new int[7];
     public static int[] oszlop2 = new int[7];
     public static int[] oszlop3 = new int[7];
-    public static String szinek[] = {"P", "T", "Z", "M"};
-    public static String ertekek[] = {"VIII", "IX", "X", "ALS", "KIR", "ASZ"};
+    public static String szinek[] = {"P", "T", "Z"};
+    public static String ertekek[] = {"VIII", "IX", "X", "ALS","FEL", "KIR", "ASZ"};
 
     public static void main(String[] args) {
         keveres();
@@ -18,8 +18,7 @@ public class Gondolatolvaso {
     public static String[] keveres() {
         for (int i = 0; i < szinek.length; i++) {
             for (int j = 0; j < ertekek.length; j++) {
-                pakli[i] = szinek[i];
-                pakli[j] = szinek[i] + "_" + ertekek[j];
+                pakli[i*ertekek.length+j] = szinek[i] + "_" + ertekek[j];
             }
 
         }
